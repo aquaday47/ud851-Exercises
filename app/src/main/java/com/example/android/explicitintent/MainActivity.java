580f3e9753d979b78ec85be9028a2548b6ec68cc
AquaticNewKeyHack <aaronquaday@gmail.com>
2018-02-05 18:34:40 -0800

    completed 04-03, adding Extra to Intent and retrieving in child

--- app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -54,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO (1) Retrieve the text from the EditText and store it in a variable
-
+                String userInput = mNameEntry.getText().toString();
                 /*
                  * Storing the Context in a variable in this case is redundant since we could have
                  * just used "this" or "MainActivity.this" in the method call below. However, we
@@ -74,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent startChildActivityIntent = new Intent(context, destinationActivity);
 
                 // TODO (2) Use the putExtra method to put the String from the EditText in the Intent
-
+                startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, userInput);
                 /*
                  * Once the Intent has been created, we can use Activity's method, "startActivity"
                  * to start the ChildActivity.
